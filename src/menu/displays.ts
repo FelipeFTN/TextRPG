fed7941df4cd31017722414b4d77fd06234ee116
FelipeFTN <felipe.tenorio123456@gmail.com>
2022-05-25 13:52:26 -0300

    Polishing Displays

--- src/menu/displays.ts
+++ src/menu/displays.ts
@@ -1,31 +1,29 @@
 import { titleScreenSelector } from "./main"
 
 export async function help(): Promise<void> {
-    process.stdout.write('\u001B[2J\u001B[0;0f')
-    console.log('                                 ')    
-    console.log('#################################')    
-    console.log('#    Welcome to the Text RPG    #')    
-    console.log('#################################')    
-    console.log('Use up, down, left, right to move')    
-    console.log('  Type your commands to do them  ')    
-    console.log(' Use "look" to inspect something ')    
-    console.log('     Good luck and have fun!     ')    
-    console.log('                                 ')    
-    console.log('             - Ok -              ')    
-    console.log('                                 ')    
-    console.log('    Copyright 2022 FelipeFTN     ')    
+    process.stdout.write('\u001B[2J\u001B[0;0f');
+    console.log('#################################');
+    console.log('#    Welcome to the Text RPG    #');   
+    console.log('#################################');
+    console.log('Use up, down, left, right to move');
+    console.log('  Type your commands to do them  ');
+    console.log(' Use "look" to inspect something ');
+    console.log('     Good luck and have fun!     ');
+    console.log('                                 ');
+    console.log('             - Ok -              ');
+    console.log('                                 ');
+    console.log('    Copyright 2022 FelipeFTN     ');
     await titleScreenSelector();
 
 }
 export async function title(): Promise<void> {
-    process.stdout.write('\u001B[2J\u001B[0;0f')
-    console.log('                                ')    
-    console.log('################################')    
-    console.log('#    Welcome to the Text RPG   #')    
-    console.log('################################')    
-    console.log('            - Play -            ')    
-    console.log('            - Help -            ')    
-    console.log('            - Quit -            ')    
-    console.log('    Copyright 2022 FelipeFTN    ')    
+    process.stdout.write('\u001B[2J\u001B[0;0f');
+    console.log('################################');
+    console.log('#    Welcome to the Text RPG   #');
+    console.log('################################');
+    console.log('            - Play -            ');
+    console.log('            - Help -            ');
+    console.log('            - Quit -            ');
+    console.log('    Copyright 2022 FelipeFTN    ');
     await titleScreenSelector();
 }
